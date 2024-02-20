6b7fa8147caffcbc81c8b3aa1eb4fac36507a1c1
Reagan Otieno <oluochreagano@gmail.com>
2024-02-20 12:41:03 -0800

    html file

---
+++ style.js
@@ -0,0 +1,8 @@
+ div {
+           background-color:rgba(0, 0, 255, 0.2);
+           border-top: solid blue;
+           border-right: dashed red;
+           border-bottom: dotted green;
+           border-right: solid black;
+           box-sizing:border-box;
+        }
